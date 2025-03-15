55aacae0128491d5942d8fc28a35a1464186e401
Corentin-Legrand <corentin.legrand@iut-dijon.u-bourgogne.fr>
2025-03-15 23:32:06 +0100

    menu reworked

--- lib/screens/welcome_page.dart
+++ lib/screens/welcome_page.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 import 'home_screen.dart';
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
+
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 7),
+    )..repeat(reverse: true);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +36,7 @@ class WelcomePage extends StatelessWidget {
           // L'image en arrière-plan
           Positioned.fill(
             child: Image.asset(
-              "assets/images/fond_ecran_principal.jpg",
+              "assets/images/background_animated.gif",
               fit: BoxFit.cover, // L'image couvre toute la surface
             ),
           ),
@@ -21,14 +45,32 @@ class WelcomePage extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.only(top: 50.0), // Décaler vers le haut
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.start, // Aligner le contenu vers le haut
+                mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: <Widget>[
-                  Container( // Espace autour du texte
-                    child: const Text(
-                      'Screen Breaker',
-                      style: TextStyle(fontSize: 200, color: Colors.pink, fontFamily: 'Penstand'),
-                    ),
+                  AnimatedBuilder(
+                    animation: _controller,
+                    builder: (context, child) {
+                      double moveY = sin(_controller.value * 2 * pi) * 20;
+                      double rotate = sin(_controller.value * 2 * pi) * 0.05;
+
+                      return Transform.translate(
+                        offset: Offset(0, moveY),
+                        child: Transform.rotate(
+                          angle: rotate,
+                          origin: const Offset(100, 100), // Centre de rotation
+                          child: const Text(
+                            'Screen Breaker',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 200,
+                              color: Colors.pink,
+                              fontFamily: 'Penstand',
+                            ),
+                          ),
+                        ),
+                      );
+                    },
                   ),
                   const SizedBox(height: 20),
                   TextButton(
@@ -42,9 +84,9 @@ class WelcomePage extends StatelessWidget {
                     },
                     child: Container(
                       alignment: Alignment.center, // Centrer le texte
-                      width: 200, // Largeur du bouton
+                      width: 200,
                       height: 150,
-                      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 30.0), // Ajouter du padding
+                      padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 30.0),
                       decoration: BoxDecoration(
                         image: DecorationImage(
                           image: AssetImage('assets/images/bouton_update.png'),
@@ -55,7 +97,7 @@ class WelcomePage extends StatelessWidget {
                         style: TextStyle(
                           fontSize: 20,
                           fontFamily: 'terminal',
-                          color: Colors.white, // Le texte en blanc
+                          color: Colors.white,
                         ),
                       ),
                     ),
